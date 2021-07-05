e098b2b52817c3f0cf1b4713f7d6adee608bc5a2
HuOneNumber <70044508+HuOneNumber@users.noreply.github.com>
2021-07-05 21:32:52 +0800

    Update index.py

--- index.py
+++ index.py
@@ -300,13 +300,11 @@ class Util:  # 统一的类
                   "UID_a8gnLryfAK4WfqNKYpWPDCSbQ10A"
               ]
         }
-        headers = {'content-type': 'application/json'}
+        headers = {'Content-Type:application/json'}
         try:
             res = requests.post('http://wxpusher.zjiecode.com/api/send/message',data = data, headers = headers)
         finally: 
             Util.log(res.text)
-            Util.log(res.json())
-            Util.log('调用了')
     @staticmethod
     def GenDeviceID(username):
         # 生成设备id，根据用户账号生成,保证同一学号每次执行时deviceID不变，可以避免辅导员看到用新设备签到
